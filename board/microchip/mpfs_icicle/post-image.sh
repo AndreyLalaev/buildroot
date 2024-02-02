8eb36634416c1d95e9710a41113292855b98033f
Andrew Lalaev <andrey.lalaev@gmail.com>
2024-02-02 11:17:58 +0100

    mpfs_icicle: use barebox instead of uboot

--- board/microchip/mpfs_icicle/post-image.sh
+++ board/microchip/mpfs_icicle/post-image.sh
@@ -6,8 +6,8 @@ MKIMAGE="${HOST_DIR}"/bin/mkimage
 BOARD_DIR="$(pwd)"/"${0%/*}"
 
 pushd "${BINARIES_DIR}"
-"${HSS_PAYLOAD_GENERATOR}" -c "${BOARD_DIR}"/config.yaml payload.bin
-cp "${BOARD_DIR}"/mpfs_icicle.its "${BINARIES_DIR}"/mpfs_icicle.its
-"${MKIMAGE}" -f mpfs_icicle.its mpfs_icicle.itb
+#"${HSS_PAYLOAD_GENERATOR}" -c "${BOARD_DIR}"/config.yaml payload.bin
+#cp "${BOARD_DIR}"/mpfs_icicle.its "${BINARIES_DIR}"/mpfs_icicle.its
+#"${MKIMAGE}" -f mpfs_icicle.its mpfs_icicle.itb
 popd
 support/scripts/genimage.sh -c "${BOARD_DIR}"/genimage.cfg
